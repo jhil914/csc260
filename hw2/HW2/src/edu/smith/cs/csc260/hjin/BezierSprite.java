54c7773d852d93c52aa86b578936f36efdc810f1
Hee Jin <hjin@smith.edu>
2013-09-22 21:04:42 -0400

    Final submission for HW2

--- hw2/HW2/src/edu/smith/cs/csc260/hjin/BezierSprite.java
+++ hw2/HW2/src/edu/smith/cs/csc260/hjin/BezierSprite.java
@@ -9,25 +9,25 @@ import edu.smith.csc.csc260.spites.Sprite;
 import edu.smith.csc.csc260.util.Point;
 /**
  * 
- * @author Alice Jin
- * 
- * xt= (1-t)2x0 +2t(1-t)x1 + t2x2 
-yt= (1-t)2y0 +2t(1-t)y1 + t2y2 
+@author Hee Jin
+CSC 260 
+22 Sep 2013
 
+initializes the bezier curve
  *
  */
 public class BezierSprite extends Sprite{
-	PShape bezier ;
-
-	/**
-	 * 
-	 * @param p1
-	 * @param p2
-	 * @param p3
-	 * @param sub
-	 */
+	// initialize bezier as pshape
+	PShape bezier ; 
+	/** initilize sub*/
 	double sub;
+	/** initilize the points*/
 	Point p1,p2,p3;
+	/**
+	 * Constructor for the Bezier curve
+	 
+	 */
+	
 	public BezierSprite(Point p1, Point p2, Point p3, double d){
 		this.sub = d;
 		this.p1 = p1;
@@ -36,19 +36,17 @@ public class BezierSprite extends Sprite{
 	}
 
 
-
+/** renders the bezier curve with the formula*/
 	public void render(SmithPApplet pApplet){
 	
 		if(bezier == null) {
 			
-			bezier= pApplet.createShape(); 
+			bezier= pApplet.createShape(); // for some reason this syntax doesn't work...
 			
-			bezier.beginShape();
+			bezier.beginShape();//gives me a nullpointererror
 			
 			for(float t = 0; t <= 1.0; t+= 1.0/sub) {
-				//		for(float i=p1.getX();i<=p3.getX();i++){
-				///			for(float j=p1.getY();i<=p3.getY();j++){
-
+				
 				int xt= (int) ((1-t)*2*p1.getX() +2*t*(1-t)*p2.getX() + 2*t*p3.getX()); 
 				int yt= (int) ((1-t)*2*p1.getY() +2*t*(1-t)*p2.getY() + 2*t*p3.getY());
 				
--- hw2/HW2/src/edu/smith/cs/csc260/hjin/HW2Applet.java
+++ hw2/HW2/src/edu/smith/cs/csc260/hjin/HW2Applet.java
@@ -6,7 +6,13 @@ import edu.smith.csc.csc260.spites.RectangleSprite;
 import edu.smith.csc.csc260.util.Color;
 import edu.smith.csc.csc260.util.Point;
 
-
+/**@author Hee Jin
+ * CSC 260
+ * 22 Sep 2013
+ * 
+ *  Application for the Bezier curve
+ *  draws the curve on the applet
+ * */
 public class HW2Applet extends SmithPApplet{
 /**
 	 * 
@@ -16,11 +22,12 @@ public class HW2Applet extends SmithPApplet{
 	public HW2Applet(){
 		
 	}
+	/** sets up the bezier curve object*/
 	public void setup(){
 		
 		size(100,100);
 		
-		System.out.println("c");
+		
 		Point a = new Point(3,3);
 		Point b = new Point(4,4);
 		Point c = new Point (5,5);
@@ -30,6 +37,7 @@ public class HW2Applet extends SmithPApplet{
 		
 		
 	}
+	/** draws it out */
 	public void draw(){
 	
 		background(200,20,100);
